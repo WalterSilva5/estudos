1ae45eb31761563d8718ea53c721922ae7a0c66e
waltersilva5 <waltersilva5@outlook.com>
2019-04-08 10:52:59 -0300

    adicionado conteudo de AED

--- uast/AED/meusAlgoritmos/estruturas/Lista.py
+++ uast/AED/meusAlgoritmos/estruturas/Lista.py
@@ -19,21 +19,50 @@ class Lista(object):
             perc.proximo = no
         self.tamanho +=1
 
+    def pop(self, indice=None):
+        """ao não passar um indice para o metodo pop ele ira remover o ultimo
+            elemento da lista e em seguida removelo"""
+        print("lista antiga:\n")
+        print(self.__str__())
+        perc = self.inicio
+        ultimo = self.inicio
+        if indice == None:
+            while perc.proximo is not None:
+                perc = perc.proximo
+                ultimo = perc.elemento
+            perc.elemento=None
+        else:
+           """ao passar um parametro o metodo pop ira remover o elemento no
+               indice definido"""
+           while perc.indice is not None:
+                if(perc.indice == indice):
+                    ultimo = perc.indice
+                    perc = None
+                    break
+                perc = perc.proximo
+        print("elemento removido: {}\n".format(ultimo))
+        print("nova lista:\n")
+        print(self.__str__())
+    def append(self):
+        pass
+
     def __len__(self):
         return self.tamanho
     def __str__(self):
-        result = "["
+        result = "[{}, ".format(self.inicio.elemento)
         perc = self.inicio
         while perc.proximo is not None:
-            perc = perc.proximo
-            result+=", {}".format(.elemento)
+            perc=perc.proximo
+            result+="{}, ".format(perc.elemento)
+
+        result = result[:-2]
         result+="]"
         return result
 
 ll = Lista()
+
 ll.add("a")
 ll.add("b")
 ll.add("c")
 ll.add("d")
-
-print(ll)
+ll.pop()
