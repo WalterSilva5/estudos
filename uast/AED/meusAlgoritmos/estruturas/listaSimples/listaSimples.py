ff0504248f1e35972cb12836914ef89809ec41b8
waltersilva5 <waltersilva3@outlook.com>
2019-09-11 11:01:55 -0300

    adicionado lista simplesmente encadeada

---
+++ uast/AED/meusAlgoritmos/estruturas/listaSimples/listaSimples.py
@@ -0,0 +1,24 @@
+class Lista():
+    def __init__(self):
+        self.__inicio = None
+        self.__tam = 0
+        
+    class No:
+        def __init__(self, valor):
+            self.valor = valor
+            self.proximo = None
+            
+    def adicionar(self, valor):
+        no = self.No(valor)
+        
+        if self.__inicio is None:
+            self.__inicio = no
+        else:
+            perc = self.__inicio
+            while perc.proximo is not None:
+                perc=perc.proximo
+            perc.proximo = no
+        self.__tam +=1
+        
+    def __str__(self):
+        pass
