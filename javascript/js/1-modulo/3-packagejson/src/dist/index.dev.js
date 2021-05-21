9571fc5138937b70601c5f5f3a4e1539830389d5
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-05-20 23:08:19 -0300

    alterado curso js

---
+++ javascript/js/1-modulo/3-packagejson/src/dist/index.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+/* eslint-disable no-console */
+var nome = 'walter';
+var valor = 24;
+var altura = 1.7;
+nome = 'jose';
+console.log(nome + valor + altura);
---
+++ javascript/js/1-modulo/3-packagejson/src/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+let nome = 'walter';
+const valor = 24;
+const altura = 1.7;
+nome = 'jose';
+console.log(nome + valor + altura);
